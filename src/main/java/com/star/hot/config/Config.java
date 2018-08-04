31fc44169cc4e0040251cce953d0015815565e98
Devraj Jaiman <devraj@furlenco.com>
2018-08-04 12:52:17 +0530

    first commit

---
+++ src/main/java/com/star/hot/config/Config.java
@@ -0,0 +1,7 @@
+package com.star.hot.config;
+
+/**
+ * Created by devraj on 04/08/18.
+ */
+public class Config {
+}
---
+++ src/main/java/com/star/hot/logging/Logging.java
@@ -0,0 +1,7 @@
+package com.star.hot.logging;
+
+/**
+ * Created by devraj on 04/08/18.
+ */
+public class Logging {
+}
